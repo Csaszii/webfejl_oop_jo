9b86884335aab3727bed4f3f2e23fc3017b51b33
Csaszar Oliver <csaszar.oliver@bolyai.local>
2025-01-17 10:06:00 +0100

    mi legyen a commit

---
+++ arraylist/main.js
@@ -0,0 +1,51 @@
+class ArrayList {
+    /**
+     * @type {Number}
+    */
+    #currentLenght
+    #currentState
+
+    get Count(){
+        return this.#currentLenght
+    }
+
+    constructor() {
+        this.#currentLenght = 0;
+        this.#currentState = {};
+    }
+
+    Add(item) {
+        const index = this.#currentLenght;
+        this.#currentState[index] = item;
+        Object.defineProperty(this, index, { 
+            get: function () {
+                return this.#currentState;
+            },
+            set: function(value) {
+                this.#currentState[index] = value;
+            }
+        })
+        this.#currentLenght++
+    }
+
+    Clear() {
+        this.#currentLenght = 0;
+        this.#currentState = {};
+    }
+}
+
+const alma = [];
+
+Object.defineProperty(alma, "nev", { value: "Ferenc"})
+alma.nev = 'asd';
+console.log(alma);
+
+const a = new ArrayList();
+
+a.Add(5);
+console.log(igen[0]);
+
+a.Add("wdqjöqwdiöwdqjiö");
+a.Add({new: "odwdqwddqw"})
+
+console.log(a); 
